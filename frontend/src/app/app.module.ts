fca8f50c272ee73c642de1f660f5746952690f3a
Riya-gupta30 <riya.gupta@call-ai.com>
2023-06-20 10:11:45 +0530

    add routing and create components for ecommerce webapplication

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    AppNavComponent,
+    CartComponent,
+    CheckoutComponent,
+    PagenotfoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CartComponent } from './cart/cart.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+export const APP_ROUTES: Routes = [
+    { path: 'product', component: ProductListComponent },
+    { path: 'cart', component: CartComponent },
+    { path : 'checkout', component: CheckoutComponent },
+    { path: '',  redirectTo: 'products', pathMatch: 'full' },
+    { path: '**', component: PagenotfoundComponent},
+];
