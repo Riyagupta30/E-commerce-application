2f5f6cac4d4760a1d956f431fa4bc7fc43173969
Riya-gupta30 <riya.gupta@call-ai.com>
2023-06-24 12:24:50 +0530

    save product-list data to db

--- frontend/server/app.js
+++ frontend/server/app.js
@@ -4,11 +4,75 @@ const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
 const config = require('./config');
+const Product = require('./models/product');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.mongoURL,
     { useNewUrlParser: true }
 );
+
+app.get('/seeddb', (req, res) => {
+    const data = [
+        {
+            _id: "5dgd26327usyri8f84fhr77",
+            name: "Javascript Mobile Application Development",
+            description: "Wheather you are developing a mobile app for ipad or on a Windows Phones",
+            image: "/assets/one.jpg",
+            price: 10,
+            __v: 0
+        },
+        {
+            id: "5dgd26327usyri8f84fhr77",
+            name: "Javascript Mobile Application Development",
+            description: "Wheather you are developing a mobile app for ipad or on a Windows Phones",
+            image: "/assets/one.jpg",
+            price: 10,
+            __v: 0
+        },
+        {
+            _id: "5dgd26327usyri8f84fhr77",
+            name: "Javascript Mobile Application Development",
+            description: "Wheather you are developing a mobile app for ipad or on a Windows Phones",
+            image: "/assets/one.jpg",
+            price: 10,
+            __v: 0
+        },
+        {
+            id: "5dgd26327usyri8f84fhr77",
+            name: "Javascript Mobile Application Development",
+            description: "Wheather you are developing a mobile app for ipad or on a Windows Phones",
+            image: "/assets/one.jpg",
+            price: 10,
+            __v: 0
+        },
+        {
+            id: "5dgd26327usyri8f84fhr77",
+            name: "Javascript Mobile Application Development",
+            description: "Wheather you are developing a mobile app for ipad or on a Windows Phones",
+            image: "/assets/one.jpg",
+            price: 10,
+            __v: 0
+        },
+        {
+            id: "5dgd26327usyri8f84fhr77",
+            name: "Javascript Mobile Application Development",
+            description: "Wheather you are developing a mobile app for ipad or on a Windows Phones",
+            image: "/assets/one.jpg",
+            price: 10,
+            __v: 0
+        },
+    ];
+    data.forEach((product) => { 
+        const newProduct = new Product({
+            name: product.name,
+            description: product.description,
+            image: product.image,
+            price: product.price,            
+        });
+        newProduct.save();
+    })
+    res.send("ok")
+})
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../dist')))
